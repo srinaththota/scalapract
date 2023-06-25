436d953c94e9cfa08833c8ea13c671f347983dcd
srinaththota <srinath.thota@gmail.com>
2023-06-25 12:41:32 +0200

    jumps start

--- src/main/scala/Problem001/Problem001.scala
+++ src/main/scala/Problem001/Problem001.scala
@@ -2,5 +2,8 @@ package Problem001
 
 class Problem001 {
 // minimum jumps required to reach
-  
+  def minimumJumps(arr:Array[Int]):Int={
+
+    0
+  }
 }
--- src/main/scala/ScalaPract.scala
+++ src/main/scala/ScalaPract.scala
@@ -1,4 +1,5 @@
 object ScalaPract extends  App{
-
   println("hellooo")
+
+  //problem001 to find the minimum number of jumps
 }
