7f33bbcd1f1982a1e8b7c63ee202ebd3decbda8c
srinaththota <srinath.thota@gmail.com>
2023-06-25 12:59:17 +0200

    added code for min jumps

--- src/main/scala/Problem001/Problem001.scala
+++ src/main/scala/Problem001/Problem001.scala
@@ -3,7 +3,28 @@ package Problem001
 class Problem001 {
 // minimum jumps required to reach
   def minimumJumps(arr:Array[Int]):Int={
+    val n=arr.length-1
+    minJumps(arr,0,n)
+  }
 
-    0
+  def minJumps(arr: Array[Int], value:Int, n: Int):Int={
+    var l=value
+    var h=n
+    if(l==h){
+      0
+    }
+    if(arr(l)==0){
+      Integer.MAX_VALUE
+    }
+    var min=Integer.MAX_VALUE
+
+    for(i<- l+1 to h if i <= l+arr(l)){
+
+      val jumps=minJumps(arr,i,h)
+      if(jumps != Integer.MAX_VALUE && jumps+1 < min){
+        min=jumps+1
+      }
+    }
+    min
   }
 }
--- src/main/scala/ScalaPract.scala
+++ src/main/scala/ScalaPract.scala
@@ -1,5 +1,11 @@
+import Problem001.Problem001
+
 object ScalaPract extends  App{
-  println("hellooo")
 
   //problem001 to find the minimum number of jumps
+  val problem001Array=Array(1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9)
+  val problem001=new Problem001
+
+  val problem001Res=problem001.minimumJumps(problem001Array)
+  println(problem001Res)
 }
